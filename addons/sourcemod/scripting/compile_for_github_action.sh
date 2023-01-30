d2de8cf19ec91d7afa2ed912d9d27e4c4439875e
El Diablo <shadelinemailbox-war3evo@yahoo.com>
2023-01-30 15:15:19 -0500

    only for github action

---
+++ addons/sourcemod/scripting/compile_for_github_action.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+fullpath=${PWD};
+
+test -e compiled || mkdir compiled
+
+newfilename=${@##*/}
+
+#echo $@
+#echo $newfilename
+
+# ./spcomp
+#-t<num>  TAB indent size (in character positions, default=8)
+#-v<num>  verbosity level; 0=quiet, 1=normal, 2=verbose (default=1)
+#-;[+/-]  require a semicolon to end each statement (default=-)
+#         -O<num>  optimization level (default=-O2)
+#             0    no optimization
+#             2    full optimizations
+
+if [[ $# -ne 0 ]]
+then
+	for i in "$newfilename";
+	do
+		fullpathsourcefile="`echo $fullpath/$i`";
+		smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
+		outputfile="`echo $fullpath/compiled/$smxfile`"
+		echo -n "Single File Compiling $i...";
+		./spcomp_1.9 -t4 -v1 $fullpathsourcefile -o$outputfile
+	done
+else
+for sourcefile in *.sp
+do
+	fullpathsourcefile="`echo $fullpath/$sourcefile`";
+	smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`";
+	echo -n "All Files Compiling $sourcefile...";
+	./spcomp_1.9 -t4 -v1 $fullpathsourcefile -ocompiled/$smxfile;
+done
+fi
